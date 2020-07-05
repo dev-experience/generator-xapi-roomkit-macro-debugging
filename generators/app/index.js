448eaea85790e360e1c80fe2159c5a3858fa014d
Vlad DevExperience <vlad.dev.experience@gmail.com>
2020-07-05 23:38:24 +0200

    Implement the generator * Updated generator to copy template files

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,15 +2,26 @@ var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
     scaffold() {
-        console.log("Copying the files");
+        console.log();
+        console.log("** Copying the template files:");
+        console.log();
+
+        this.fs.copy(this.templatePath('.*'), this.destinationPath(''));
+        this.fs.copy(this.templatePath('**/*'), this.destinationPath(''));
     }
 
     install() {
-        console.log("Installing required dependencies");
+        console.log();
+        console.log("** Installing required dependencies");
+        console.log("npm install");
+        console.log();
+
         this.npmInstall();
     }
 
     end() {
-        console.log("Follow the instructions from README.md -> How to");
+        console.log();
+        console.warn("** Follow the instructions from README.md -> How to");
+        console.log();
     }
 };
